c0ca1647fd7a6c5c992c5d97a30d4b3b083075e9
ojijo123 <oumaerick173@gmail.com>
2024-10-02 22:30:45 -0700

    promises and packages

---
+++ 0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
+
+and…
+Don’t forget to run $ npm install when you have the package.json
+
+Response Data Format
+uploadPhoto returns a response with the format
+
+{
+  status: 200,
+  body: 'photo-profile-1',
+}
