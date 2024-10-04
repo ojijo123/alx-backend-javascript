09fa466e7abebe97870391bff2da21ad6a8183bf
ojijo123 <oumaerick173@gmail.com>
2024-10-04 18:39:15 +0000

    append to each other

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,16 +1,8 @@
-import getBudgetObject from './7-getBudgetObject';
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
 
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-
-  return fullBudget;
+  return newArray;
 }
