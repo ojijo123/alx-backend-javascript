c02e338c73e0646254d762e4ecc4f0ce3e0c6f17
ojijo123 <oumaerick173@gmail.com>
2024-11-26 19:39:03 +0000

    Executing basic javascript

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
